dfa1191a8c2498b2b06914e249bdc22ba6a4d267
Kelzinha000 <138785661+Kelzinha000@users.noreply.github.com>
2024-02-28 12:17:22 -0300

    Add files via upload

---
+++ js/conversor.test.js
@@ -0,0 +1,8 @@
+const calculo= require('./index');
+
+describe('Testa a função calculo', ()=>{
+  test('Verificar se 40 em fahrenheit é 104')
+   
+  const conversor = 104
+  expect(conversor).toBe(104);
+})
---
+++ js/index.js
@@ -0,0 +1,7 @@
+const { moduleExpression} = requeire("@babel/types");
+
+function conversorDeTemperatura (celsius) {
+ const calculo = (celsius * 1.8) + 32
+ return conversor
+}
+module.exports = calculo;
